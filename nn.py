04e0008d0f5e5e70bf88904be732644dd56e3d5e
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-08-24 18:06:25 +0700

    finishing up the backpropogation process

--- nn.py
+++ nn.py
@@ -9,6 +9,7 @@ class Value:
     def __repr__(self):
         return f"Value(data={self.data})"
     def __add__(self, other):
+        other = other if isinstance(Value, other) else Value(other)
         out = (Value(self.data + other.data), other, '+')
         def _backward():
             self.grad += 1.0 * out.grad
@@ -16,13 +17,29 @@ class Value:
         out._backward = _backward
         return out 
     def __mul__(self, other):
+        other = other if isinstance(Value, other) else Value(other)
         out =  (Value(self.data * other.data), other, '*')
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad 
         out._backward = _backward
         return out
+    def __pow__(self, other):
+        assert isinstance(other, (int,float)), "Only int/float is supported"
+        out = Value(self.data**other, (self, ), f'**{other}')
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+        self._backward = _backward
+        return out
 
+    def __rmul__(self, other):
+        return self*other
+    def __truediv__(self, other):
+        return self * other**-1
+    def __neg__(self):
+        return self * -1
+    def __sub__(self, other):
+        return self + (-other)
     def tanh(self):
         x = self.data
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
@@ -31,6 +48,12 @@ class Value:
             self.grad = (1 - t**2) * out.grad
         out._backward = _backward
         return out
+    def exp(self):
+        x = self.data
+        out  = Value(math.exp(x), (self, ), 'exp')
+        def _backward():
+            self.grad += out.data * out.grad
+        self._backward = _backward
     def backward(self):
         topo = []
         visited = set()
@@ -44,6 +67,7 @@ class Value:
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
+    
 
 
 
