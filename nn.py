ac18a12d8d29d4bbd196eef750b67204d9ead7b5
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-08-24 18:21:48 +0700

    start building neurons

---
+++ nn.py
@@ -0,0 +1,7 @@
+from engine import Value
+import random
+class Neuron:
+
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1,1))
