a37e6e2e6cc450550b6f0337bd51ff2127cfaf43
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-08-24 18:47:31 +0700

    finished mlp

--- nn.py
+++ nn.py
@@ -16,3 +16,14 @@ class Layer:
     
     def __call__(self, x):
         outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs)==1 else outs 
+
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i],sz[i+1]) for i in range(len(nouts))]
+    
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer
+        return x
