7b4b3fbdbbf86f927cc6f3925ba0f1c3dc8caf4d
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-08-26 17:36:30 +0700

    finished parameters

--- nn.py
+++ nn.py
@@ -9,7 +9,7 @@ class Neuron:
         act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
         out = act.tanh()
         return out
-    def parameter(self):
+    def parameters(self):
         return self.w + [self.b]
 
 class Layer:
@@ -19,10 +19,8 @@ class Layer:
     def __call__(self, x):
         outs = [n(x) for n in self.neurons]
         return outs[0] if len(outs)==1 else outs 
-    def parameter(self):
-        params = []
-        for neuron in self.neurons:
-            pass
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
 
 
 class MLP:
@@ -34,3 +32,6 @@ class MLP:
         for layer in self.layers:
             x = layer
         return x
+    
+    def paremeters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
